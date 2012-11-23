9a10799c64fd577e7f1dfba05b82e4efad5aecc8
Sean Ochoa <sean.m.ochoa@gmail.com>
2012-11-23 15:23:21 -0800

    Update to add includes to main.  Still broken.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>;
 using namespace std;
 
+#include "sort/helper/list.hpp"
+#include "sort/merge.hpp"
+
 int main(int argc, char** argv){
-    sorted::list<int>* l = get_random_list();
-    l = merge_sort(l);
+    helper::list<int>* l = sorted::get_random_list();
+    l = sorted::merge_sort(l);
     for (int i = 0; i < (l->length() - 1); i++){
         int t = l->get(i);
         int u = l->get(i+1); 
         if (t > u){
-            sorted::list<int>* m = l->slice(i - 5, i + 5);
+            helper::list<int>* m = l->slice(i - 5, i + 5);
             cout << m << endl;
             delete m;
             break;
