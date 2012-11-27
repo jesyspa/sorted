0f3b49e0a572ce829c0ba3157b88f16759e8916a
Sean Ochoa <sean.m.ochoa@gmail.com>
2012-11-27 12:23:11 -0800

    Added exception handling at the top level.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <exception>
 using namespace std;
 
 #include "sort/merge.hpp"
 
 int main(int argc, char** argv){
-    std::vector<int> v;
-    v = sorted::get_random_vector(10, 100);
-    cout << "Unsorted:  " << v << endl;
-    std::vector<int> v_sorted = sorted::merge_sort(v);
-    cout << "Sorted:    " << v_sorted << endl;
+
+    vector<int> v;
+
+    try{
+        v = sorted::get_random_vector(10, 100);
+        cout << "Unsorted:  " << v << endl;
+    }catch(exception& e){
+        cerr << "get_random_vector(int count,int base) ERROR:  " << e.what() << endl;
+        return -1;
+    }
+
+    try{
+        vector<int> v_sorted = sorted::merge_sort(v);
+        cout << "Sorted:    " << v_sorted << endl;
+    }catch(exception& e){
+        cerr << "Merge Sort ERROR:  " << e.what() << endl;
+        return -1;
+    }
+
     return 0;
 }
