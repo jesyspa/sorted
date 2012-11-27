1de2286f9a229076702e31c5f9850cbc5c30aee0
Anton Golov <jesyspa@gmail.com>
2012-11-27 22:40:23 +0100

    Optimised and cleaned up.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,10 +2,30 @@
 
 #include <iostream>
 #include <exception>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, char** argv){
+// I'm surprized this doesn't already exist.  Or,
+// more likey, I'm doing this wrong.  lol.
+template <typename T>
+std::ostream& operator<<(std::ostream& o, std::vector<T> const& v) {
+    o << "[";
+    bool first = true;
+    for (auto const& e : v) {
+        if (!first)
+            o << ", ";
+        o << e;
+        first = false;
+    }
+    o << "]";
+    return o;
+}
+
+int main() {
     std::vector<int> v;
-    
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+
     // What can this throw?
     try {
         v = sorted::get_random_vector(10, 100);
@@ -17,9 +37,9 @@ int main(int argc, char** argv){
 
     // Why are you still trying to sort even when you know you don't have a vector?
     try {
-        auto v_sorted = sorted::merge_sort(v);
-        std::cout << "Sorted:    " << v_sorted << std::endl;
-    } catch(exception& e) {
+        sorted::merge_sort(v.begin(), v.end());
+        std::cout << "Sorted:    " << v << std::endl;
+    } catch(std::exception& e) {
         std::cerr << "Merge Sort ERROR:  " << e.what() << std::endl;
         return -1;
     }
--- cpp/sort/merge.hpp
+++ cpp/sort/merge.hpp
@@ -3,77 +3,47 @@
 
 #include <vector>
 #include <utility>
-#include <cstdlib>             /* srand()  */
-#include <ctime>               /* time()   */
-
-// I'm surprized this doesn't already exist.  Or, 
-// more likey, I'm doing this wrong.  lol.  
-template <typename T>
-std::ostream& operator<<(std::ostream& o, std::vector<T> const& v) {
-    unsigned int i = 0;
-    o << "[";
-    bool first = true;
-    for (auto const& e : v) {
-        if (!first)
-            o << ", ";
-        o << e;
-        first = false;
-    }
-    o << "]";
-    return o;
-};
+#include <ostream>
+#include <cstdlib>
+#include <type_traits>
 
 namespace sorted {
-
-    // This could better be done with iterator ranges.
-    // Also, vectors aren't the best data structure to use within the sort; a deque would
-    // offer better performance.  I suspect this might not really be O(n*log(n))
-    template<typename T>
-    std::vector<T> merge(std::vector<T> left, std::vector<T> right) {
-        std::vector<T> result;
-        result.reserve(left.size() + right.size());
-        while (left.size() || right.size()) {
-            if (left.size() && right.size()) {
-                if (left.front() <= right.front()) {
-                    result.push_back(left.front());
-                    left.erase(left.begin());
-                } else {
-                    result.push_back(right.front());
-                    right.erase(right.begin());
-                }
-            } else if (!left.empty()) {
-                result.push_back(left.front());
-                left.erase(left.begin());
-            } else if (!right.empty()) {
-                result.push_back(right.front());
-                right.erase(right.begin());
+    template<typename FwIter1, typename FwIter2, typename OIter>
+    OIter merge(FwIter1 b1, FwIter1 e1, FwIter2 b2, FwIter2 e2, OIter o) {
+        while (b1 != e1 || b2 != e2) {
+            if (b1 != e1 && b2 != e2) {
+                if (*b1 < *b2)
+                    *o++ = *b1++;
+                else
+                    *o++ = *b2++;
+            } else if (b1 != e1) {
+                o = std::copy(b1, e1, o);
+                break;
+            } else {
+                o = std::copy(b2, e2, o);
+                break;
             }
         }
-        return result;
+        return o;
     }
 
-    template<typename T>
-    std::vector<T> merge_sort(std::vector<T> in){
-
-        size_t len = in.size();
-
-        if (len < 2) return in;
-
-        std::vector<T> left, right;
-
-        if (in.size() > 2){
-            left = std::vector<T>(in.begin(), in.begin() + (len / 2));
-            right = std::vector<T>(in.begin() + (len / 2), in.end());
-        } else {
-            left.push_back(in[0]);
-            right.push_back(in[1]);
+    template<typename RAIter>
+    void merge_sort(RAIter b, RAIter e) {
+        if (e - b < 2) return;
+        if (e - b == 2) {
+            if (b[0] > b[1])
+                std::swap(b[0], b[1]);
+            return;
         }
 
-        left = merge_sort(std::move(left));
-        right = merge_sort(std::move(right));
+        // Homework: why not (2*b - e)/2 + 1?
+        auto half = b + (e-b)/2;
+        merge_sort(b, half);
+        merge_sort(half, e);
 
-        std::vector<T> out = merge(std::move(left), std::move(right));
-        return out;
+        std::vector<typename std::remove_reference<decltype(*b)>::type> result(e-b);
+        merge(b, half, half, e, result.begin());
+        std::copy(result.begin(), result.end(), b);
     }
 
     std::vector<int> get_random_vector(int count, int base) {
